29ab595afe616384be4b9e8294eeebb1834e5dd1
Stefan Bendrin <sbendrin91@gmail.com>
2019-07-16 19:31:58 +0200

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public BallConfig ballConfig;
+
+    [HideInInspector] public Rigidbody2D body;
+
+    private void Awake() {
+        body = GetComponent<Rigidbody2D>();
+
+        ApplyConfig(ballConfig);
+    }
+
+    private void ApplyConfig(BallConfig config) {
+        body.drag = config.drag;
+        body.gravityScale = config.gravityScale;
+        body.freezeRotation = config.freezeRotation;
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/BallConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Configs/Ball/Default")]
+public class BallConfig : ScriptableObject {
+
+    public float drag = 0.1f;
+    public float gravityScale = 4f;
+    public bool freezeRotation = true;
+
+    public bool test;
+}
