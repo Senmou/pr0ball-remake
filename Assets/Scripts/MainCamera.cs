1604ad432f60f4ae2e34fe8ec33020cedcc6a2f5
Stefan Bendrin <sbendrin91@gmail.com>
2019-07-17 08:43:41 +0200

    Clean up code

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -7,31 +7,22 @@ public class MainCamera : MonoBehaviour {
     public Transform wallLeft;
     public Transform wallRight;
 
-    private void Update() {
+    private void Awake() {
 
+        // Outer bounds of the walls
         float levelHeight = top.position.y - ground.position.y + top.localScale.y + ground.localScale.y;
         float levelWidth = wallRight.position.x - wallLeft.position.x + wallRight.localScale.x + wallLeft.localScale.x;
 
         float unitsPerPixelWidth = levelWidth / Screen.width;
-        float unitsPerPixelHeight = levelHeight / Screen.height;
-
         float desiredHalfHeight = 0.5f * unitsPerPixelWidth * Screen.height;
         float desiredHalfWidth = 0.5f * levelHeight;
 
-        
-
-        float verticalExtend = Camera.main.orthographicSize * 2f;
-        float horizontalExtend = 2f * Camera.main.orthographicSize * Screen.width / Screen.height;
-
         float levelAspect = levelWidth / levelHeight;
 
-        if(Camera.main.aspect < levelAspect) {
+        if (Camera.main.aspect < levelAspect) {
             Camera.main.orthographicSize = desiredHalfHeight;
         } else {
             Camera.main.orthographicSize = desiredHalfWidth;
         }
-
-        Debug.Log("levelAspect: " + levelAspect);
-        Debug.Log("aspect: " + Camera.main.aspect);
     }
 }
