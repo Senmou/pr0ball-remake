bb2690abbeaca9efd3e099020ca6cb9536ff3d55
Stefan Bendrin <sbendrin91@gmail.com>
2019-07-17 09:44:16 +0200

    Main menu scene with start and options button

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void OnClickStartButton() {
+        SceneManager.LoadScene(1);
+    }
+}
