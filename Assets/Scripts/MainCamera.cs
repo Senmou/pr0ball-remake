8a97092bd20432220decc4ec6ea4939b7c2914ec
Stefan Bendrin <sbendrin91@gmail.com>
2019-07-17 07:29:33 +0200

    Scale camera to level width

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+    public Transform wallLeft;
+    public Transform wallRight;
+
+    private void Update() {
+
+        float levelWidth = wallRight.position.x - wallLeft.position.x + wallRight.localScale.x + wallLeft.localScale.x;
+
+        float unitsPerPixel = levelWidth / Screen.width;
+
+        float desiredHalfHeight = 0.5f * unitsPerPixel * Screen.height;
+
+        Camera.main.orthographicSize = desiredHalfHeight;
+    }
+}
