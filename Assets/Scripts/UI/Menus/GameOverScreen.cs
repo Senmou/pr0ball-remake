d984953e84ac8b7ceda0cde6fdfbd363ff851729
Stefan Bendrin <sbendrin91@gmail.com>
2020-03-08 19:35:48 +0100

    Added more statistics to game over screen

--- Assets/Scripts/UI/Menus/GameOverScreen.cs
+++ Assets/Scripts/UI/Menus/GameOverScreen.cs
@@ -11,9 +11,18 @@ public class GameOverScreen : CanvasController {
     private PauseBackground pauseBackground;
 
     // Statistics
+
+    // Balls
+    private TextMeshProUGUI ballsCollisionsUI;
+    private TextMeshProUGUI ballsTotalDamageUI;
+    private TextMeshProUGUI ballsFiredUI;
+
+    // Benitrat0r
     private TextMeshProUGUI totalBetsUI;
     private TextMeshProUGUI averageBetUI;
-    private TextMeshProUGUI collisionsUI;
+
+    // Enemies
+    private TextMeshProUGUI enemiesKilledUI;
 
     private void Awake() {
         moveUI = GetComponent<MoveUI>();
@@ -21,27 +30,46 @@ public class GameOverScreen : CanvasController {
         playtimeUI = transform.FindChild<TextMeshProUGUI>("Highscore/Playtime");
         highscoreUI = transform.FindChild<TextMeshProUGUI>("Highscore/Value");
 
-        totalBetsUI = transform.FindChild<TextMeshProUGUI>("Statistics/Values/TotalBets/Value");
-        averageBetUI = transform.FindChild<TextMeshProUGUI>("Statistics/Values/AverageBet/Value");
-        collisionsUI = transform.FindChild<TextMeshProUGUI>("Statistics/Values/Collisions/Value");
+        // Balls
+        ballsCollisionsUI = transform.FindChild<TextMeshProUGUI>("Statistics/Table/BallStatistics/Collisions/Value");
+        ballsTotalDamageUI = transform.FindChild<TextMeshProUGUI>("Statistics/Table/BallStatistics/TotalDamage/Value");
+        ballsFiredUI = transform.FindChild<TextMeshProUGUI>("Statistics/Table/BallStatistics/Fired/Value");
+
+        // Benitrat0r
+        totalBetsUI = transform.FindChild<TextMeshProUGUI>("Statistics/Table/BenitratorStatistics/TotalBets/Value");
+        averageBetUI = transform.FindChild<TextMeshProUGUI>("Statistics/Table/BenitratorStatistics/AverageBet/Value");
+
+        // Enemies
+        enemiesKilledUI = transform.FindChild<TextMeshProUGUI>("Statistics/Table/EnemyStatistics/Kills/Value");
 
         pauseBackground = FindObjectOfType<PauseBackground>();
     }
 
+    private void UpdateStatisticsUI() {
+
+        // Balls
+        ballsCollisionsUI.text = Statistics.Instance.balls.collisions.ToString();
+        ballsTotalDamageUI.text = Statistics.Instance.balls.damageDealt.ToString();
+        ballsFiredUI.text = Statistics.Instance.balls.fired.ToString();
+
+        // Benitrat0r
+        totalBetsUI.text = Statistics.Instance.benitrator.totalBets.ToString();
+        averageBetUI.text =  Statistics.Instance.benitrator.AverageBet.ToString("0.##");
+
+        // Enemies
+        enemiesKilledUI.text = Statistics.Instance.enemies.killed.ToString();
+    }
+
     public override void Show() {
         SaveHighscore();
-
-        collisionsUI.text = Statistics.Instance.balls.collisions.ToString();
-        totalBetsUI.text = Statistics.Instance.benitrator.totalBets.ToString();
-        averageBetUI.text = Statistics.Instance.benitrator.AverageBet.ToString();
-
+        UpdateStatisticsUI();
         pauseBackground.disableInteractability = true;
         moveUI.FadeTo(new Vector2(0f, 0f), 0.5f);
     }
 
     public override void Hide() {
         pauseBackground.disableInteractability = false;
-        moveUI.FadeTo(new Vector2(0f, 40f), 0.5f, true);
+        moveUI.FadeTo(new Vector2(0f, 55f), 0.5f, true);
     }
 
     private void SaveHighscore() {
