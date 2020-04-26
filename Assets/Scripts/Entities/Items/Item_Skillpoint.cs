a5023a7d58346cc56e36390a56733a422a3825d1
Stefan Bendrin <sbendrin91@gmail.com>
2020-04-26 17:02:25 +0200

    Removed obsolete particle systems from items

--- Assets/Scripts/Entities/Items/Item_Skillpoint.cs
+++ Assets/Scripts/Entities/Items/Item_Skillpoint.cs
@@ -4,19 +4,12 @@ using TMPro;
 public class Item_Skillpoint : BaseItem {
 
     private TextMeshProUGUI valueUI;
-    private new ParticleSystem particleSystem;
 
     private new void Awake() {
         base.Awake();
         entityType = CurrentLevelData.EntityType.ItemSkillPoint;
-        particleSystem = GetComponent<ParticleSystem>();
 
         valueUI = transform.FindChild<TextMeshProUGUI>("Value");
-
-        if (PersistentData.instance.enableParticleSystems)
-            particleSystem.Play();
-        else
-            particleSystem.Stop();
 
         int random = Random.Range(0, 100);
         if (random < 3)
@@ -25,13 +18,6 @@ public class Item_Skillpoint : BaseItem {
             SetValue(Random.Range(1, 4), valueUI);
 
         EventManager.StartListening("ToggleParticleSystems", OnToggleParticleSystems);
-    }
-
-    private void OnToggleParticleSystems() {
-        if (PersistentData.instance.enableParticleSystems)
-            particleSystem.Play();
-        else
-            particleSystem.Stop();
     }
 
     protected override void OnItemCollected() {
